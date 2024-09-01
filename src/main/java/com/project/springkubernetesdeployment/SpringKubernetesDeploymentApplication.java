c5246828476a8abe47995906451c39f9fb11aa5b
broxzx <illyasedelnikov123@gmail.com>
2024-09-01 21:52:18 +0200

    init commit

---
+++ src/main/java/com/project/springkubernetesdeployment/SpringKubernetesDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.project.springkubernetesdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKubernetesDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKubernetesDeploymentApplication.class, args);
+    }
+
+}
